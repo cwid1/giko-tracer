f738f8e1afd92c97189c107d033a467ccefbdf47
Jarrod (Zi Wei) SU <z5592969@ad.unsw.edu.au>
2024-09-07 02:02:26 +1000

    feat: implement glyph maps, implement charset.txt that loads only relevant glyphs, separate giko_glyph to giko_bitmap and giko_glyph

--- main.c
+++ main.c
@@ -5,67 +5,88 @@
 #include <stdint.h>
 #include <assert.h>
 
-typedef struct {
-    long codepoint;
-    int advance;
+typedef struct giko_bitmap {
+    int width;
     int pitch;
     int rows;
-    uint8_t *bitmap;
+    uint8_t *data;
+} giko_bitmap_t;
+
+typedef struct giko_glyph {
+    long codepoint;
+    int advance;
+    giko_bitmap_t *bitmap;
+    struct giko_glyph *next;
 } giko_glyph_t;
 
+typedef struct giko_advance_map {
+    int size;
+    giko_glyph_t **advances;
+} giko_glyph_map_t;
+
+typedef struct giko_score {
+    long codepoint;
+    int advance;
+    double similarity;
+} giko_score_t;
+
 // Function prototypes
-giko_glyph_t *new_glyph_from_file(char *filename);
-giko_glyph_t *new_glyph_from_font_face(FT_Face face, FT_Long codepoint);
-double bitmap_similarity(giko_glyph_t *glyph_1, giko_glyph_t *glyph_2);
+giko_bitmap_t *new_image_bitmap(char *filepath);
+giko_bitmap_t *new_glyph_bitmap(FT_Face face, long codepoint);
+giko_glyph_map_t *new_glyph_map(FT_Face face, char *filename);
+giko_glyph_t *new_glyph(FT_Face face, long codepoint);
+giko_score_t best_match(giko_bitmap_t *reference, giko_glyph_t *head);
+double bitmap_similarity(giko_bitmap_t *bitmap1, giko_bitmap_t *bitmap2);
+int num_set_pixels(int8_t pixel_byte);
 int floor_frac_pixel(long fractional_pixel_count);
 int pitch_32bit(int width);
-int num_set_pixels(int8_t pixel_byte);
+void free_glyph_map(giko_glyph_map_t *map);
+void free_glyph_list(giko_glyph_t *list);
+void free_bitmap(giko_bitmap_t *bitmap);
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: ascii_tracer <fontface.ttf> <filename.bmp>\n");
+    if (argc != 4) {
+        fprintf(stderr, "Usage: ascii_tracer <fontface.ttf> <charset.txt> <reference.bmp>\n");
         return 1;
     }
-    giko_glyph_t *image = new_glyph_from_file(argv[2]);
+    giko_bitmap_t *reference = new_image_bitmap(argv[3]);
 
     FT_Library library;
     FT_Face face;
     FT_Init_FreeType(&library);
     FT_New_Face(library, argv[1], 0, &face);
-    FT_Set_Pixel_Sizes(face, image->advance, image->rows);
+    FT_Set_Pixel_Sizes(face, reference->width, reference->rows);
 
-    giko_glyph_t *glyph = new_glyph_from_font_face(face, 13247);
+    giko_glyph_map_t *glyph_map = new_glyph_map(face, argv[2]);
+    giko_score_t best = best_match(reference, glyph_map->advances[reference->width]);
+    printf("%lu, %lf\n", best.codepoint, best.similarity);
 
     FT_Done_Face(face);
     FT_Done_FreeType(library);
 
-    printf("%lf\n", bitmap_similarity(image, glyph));
-
-    free(image->bitmap);
-    free(image);
-    free(glyph->bitmap);
-    free(glyph);
+    free_bitmap(reference);
+    free_glyph_map(glyph_map);
     return 0;
 }
 
-giko_glyph_t *new_glyph_from_file(char *filename) {
-    FILE *file = fopen(filename, "rb");
+giko_bitmap_t *new_image_bitmap(char *filepath) {
+    FILE *file = fopen(filepath, "rb");
     if (!file) {
-        perror("Error opening file");
+        perror("Error opening image file");
         return NULL;
     }
 
-    giko_glyph_t *image = malloc(sizeof(giko_glyph_t));
+    giko_bitmap_t *image = malloc(sizeof(giko_bitmap_t));
 
     fseek(file, 18, SEEK_SET);
-    fread(&image->advance, 4, 1, file);
+    fread(&image->width, 4, 1, file);
     fread(&image->rows, 4, 1, file);
 
     uint32_t image_data_offset;
     fseek(file, 10, SEEK_SET);
     fread(&image_data_offset, 4, 1, file);
 
-    int pitch = pitch_32bit(image->advance);
+    int pitch = pitch_32bit(image->width);
     int image_size = pitch * image->rows;
     image->pitch = pitch;
 
@@ -86,14 +107,50 @@ giko_glyph_t *new_glyph_from_file(char *filename) {
         }
     }
 
-    image->bitmap = pixel_array;
-    image->codepoint = -1;
+    image->data = pixel_array;
 
     fclose(file);
     return image;
 }
 
-giko_glyph_t *new_glyph_from_font_face(FT_Face face, FT_Long codepoint) {
+giko_glyph_map_t *new_glyph_map(FT_Face face, char *filepath) {
+    FILE *file = fopen(filepath, "rb");
+    if (!file) {
+        perror("Error opening charset file");
+        return NULL;
+    }
+
+    giko_glyph_map_t *map = malloc(sizeof(giko_glyph_map_t));
+    int max_advance = floor_frac_pixel(face->size->metrics.max_advance);
+    map->size = max_advance;
+    map->advances = calloc(max_advance + 1, sizeof(giko_glyph_t *));
+
+    char codepoint_str[16];
+    while (fscanf(file, "%s ", codepoint_str) == 1) {
+        // PERFORMANCE: Density indexing
+        long codepoint = strtol(codepoint_str, NULL, 10);
+        giko_glyph_t *glyph = new_glyph(face, codepoint);
+        int advance = glyph->advance;
+        giko_glyph_t *head = map->advances[advance];
+        glyph->next = head;
+        map->advances[advance] = glyph;
+    }
+
+    fclose(file);
+    return map;
+}
+
+giko_glyph_t *new_glyph(FT_Face face, long codepoint) {
+    giko_glyph_t *glyph = malloc(sizeof(giko_glyph_t));
+    glyph->codepoint = codepoint;
+    glyph->bitmap = new_glyph_bitmap(face, codepoint);
+    glyph->advance = glyph->bitmap->width;
+    glyph->next= NULL;
+
+    return glyph;
+}
+
+giko_bitmap_t *new_glyph_bitmap(FT_Face face, long codepoint) {
     FT_Long glyph_index = FT_Get_Char_Index(face, codepoint);
 
     // Load and render the glyph in monochrome mode
@@ -106,12 +163,11 @@ giko_glyph_t *new_glyph_from_font_face(FT_Face face, FT_Long codepoint) {
     int pitch = pitch_32bit(advance);
 
     // Create and initialize the glyph_bitmap_t structure
-    giko_glyph_t *glyph = malloc(sizeof(giko_glyph_t));
-    glyph->codepoint = codepoint;
-    glyph->advance = advance;
-    glyph->pitch = pitch;
-    glyph->rows = face->size->metrics.y_ppem;
-    glyph->bitmap = calloc(glyph->rows * pitch, sizeof(uint8_t));
+    giko_bitmap_t *bitmap = malloc(sizeof(giko_bitmap_t));
+    bitmap->width = advance;
+    bitmap->pitch = pitch;
+    bitmap->rows = face->size->metrics.y_ppem;
+    bitmap->data = calloc(bitmap->rows * pitch, sizeof(uint8_t));
 
     // Calculate the offsets for positioning
     int ascent = floor_frac_pixel(face->size->metrics.ascender);
@@ -127,36 +183,80 @@ giko_glyph_t *new_glyph_from_font_face(FT_Face face, FT_Long codepoint) {
             int dst_x = x + x_offset;
             int dst_y = y + y_offset;
 
-            int dst_byte_index = dst_y * pitch + (dst_x / 8);
-            int dst_bit_index = dst_x % 8;
+            int in_x_bounds = (dst_x >= 0 && dst_x < advance);
+            int in_y_bounds = (dst_y >= 0 && dst_y < bitmap->rows);
 
-            int8_t src_bit_mask = (1 << (7 - src_bit_index));
-            int8_t dst_bit_mask = (1 << (7 - dst_bit_index));
+            if (in_x_bounds && in_y_bounds) {
+                int dst_byte_index = dst_y * pitch + (dst_x / 8);
+                int dst_bit_index = dst_x % 8;
 
-            if (unaligned_bitmap->buffer[src_byte_index] & src_bit_mask) {
-                glyph->bitmap[dst_byte_index] |= dst_bit_mask;
+                int8_t src_bit_mask = (1 << (7 - src_bit_index));
+                int8_t dst_bit_mask = (1 << (7 - dst_bit_index));
+
+                if (unaligned_bitmap->buffer[src_byte_index] & src_bit_mask) {
+                    bitmap->data[dst_byte_index] |= dst_bit_mask;
+                }
             }
         }
     }
 
-    return glyph;
+    return bitmap;
 }
 
-double bitmap_similarity(giko_glyph_t *glyph_1, giko_glyph_t *glyph_2) {
-    assert(glyph_1->rows == glyph_2->rows);
-    assert(glyph_1->pitch == glyph_2->pitch);
+giko_score_t best_match(giko_bitmap_t *reference, giko_glyph_t *head) {
+    giko_score_t best = {0};
+    best.advance = head->advance;
+    giko_glyph_t *curr;
+    for (curr = head; curr != NULL; curr = curr->next) {
+        double similarity = bitmap_similarity(reference, curr->bitmap);
+        // PERFORMANCE: Add greed
+        if (similarity >= best.similarity) {
+            best.similarity = similarity;
+            best.codepoint = curr->codepoint;
+        }
+    }
+
+    return best;
+}
+
+double bitmap_similarity(giko_bitmap_t *bitmap1, giko_bitmap_t *bitmap2) {
+    assert(bitmap1->rows == bitmap2->rows);
+    assert(bitmap1->pitch == bitmap2->pitch);
 
     double total_set_pixels = 0;
     double overlapping_set_pixels = 0;
 
-    for (int i = 0; i < glyph_1->pitch * glyph_1->rows; i++) {
-        int8_t byte_1 = glyph_1->bitmap[i];
-        int8_t byte_2 = glyph_2->bitmap[i];
+    for (int i = 0; i < bitmap1->pitch * bitmap1->rows; i++) {
+        int8_t byte_1 = bitmap1->data[i];
+        int8_t byte_2 = bitmap2->data[i];
         overlapping_set_pixels += num_set_pixels(byte_1 & byte_2);
         total_set_pixels += num_set_pixels(byte_1 | byte_2);
     }
 
     return overlapping_set_pixels / total_set_pixels;
+}
+
+void free_glyph_map(giko_glyph_map_t *map) {
+    for (int i = 0; i < map->size; i++) {
+        free_glyph_list(map->advances[i]);
+    }
+    free(map->advances);
+    free(map);
+}
+
+void free_glyph_list(giko_glyph_t *list) {
+    giko_glyph_t *curr;
+    giko_glyph_t *tmp = NULL;
+    for (curr = list; curr != NULL; curr = tmp) {
+        tmp = curr->next;
+        free_bitmap(curr->bitmap);
+        free(curr);
+    }
+}
+
+void free_bitmap(giko_bitmap_t *bitmap) {
+    free(bitmap->data);
+    free(bitmap);
 }
 
 int pitch_32bit(int width) {
@@ -168,6 +268,7 @@ int floor_frac_pixel(long fractional_pixel_count) {
 }
 
 int num_set_pixels(int8_t pixel_byte) {
+    // PERFORMANCE: Hashmap
     int count = 0;
     for (int i = 0; i < 8; i++) {
         if (pixel_byte & (1 << i)) {
