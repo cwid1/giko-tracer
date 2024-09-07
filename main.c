601d02c9276d8fb7d8b57f298003c8e8a37da1bc
Jarrod (Zi Wei) SU <z5592969@ad.unsw.edu.au>
2024-09-07 20:39:03 +1000

    feat: add patching and patch matching, test: add print_unicode_str to print text art

--- main.c
+++ main.c
@@ -5,10 +5,13 @@
 #include <stdint.h>
 #include <assert.h>
 
+#define MAX_ART_STR_LEN 65536
+#define LINE_FEED 10
+
 typedef struct giko_bitmap {
     int width;
     int pitch;
-    int rows;
+    int height;
     uint8_t *data;
 } giko_bitmap_t;
 
@@ -21,22 +24,30 @@ typedef struct giko_glyph {
 
 typedef struct giko_advance_map {
     int size;
+    int em_height;
     giko_glyph_t **advances;
 } giko_glyph_map_t;
 
-typedef struct giko_score {
+typedef struct giko_match {
     long codepoint;
     int advance;
     double similarity;
-} giko_score_t;
+} giko_match_t;
+
+typedef struct giko_unicode_str {
+    int size;
+    long *codepoints;
+} giko_unicode_str_t;
 
 // Function prototypes
 giko_bitmap_t *new_image_bitmap(char *filepath);
 giko_bitmap_t *new_glyph_bitmap(FT_Face face, long codepoint);
-giko_bitmap_t *crop_bitmap(giko_bitmap_t *bitmap, int x, int y, int width, int height);
 giko_glyph_map_t *new_glyph_map(FT_Face face, char *filename);
 giko_glyph_t *new_glyph(FT_Face face, long codepoint);
-giko_score_t best_match(giko_bitmap_t *reference, giko_glyph_t *head);
+giko_unicode_str_t *new_art_str(giko_bitmap_t *reference, giko_glyph_map_t *map, double greed);
+giko_match_t best_scanline_match(giko_bitmap_t *reference, giko_glyph_map_t *map, int x, int y, double greed);
+giko_bitmap_t *crop_bitmap(giko_bitmap_t *bitmap, int offset_x, int offset_y, int width, int height);
+giko_match_t patch_match(giko_bitmap_t *reference, giko_glyph_t *head);
 double bitmap_similarity(giko_bitmap_t *bitmap1, giko_bitmap_t *bitmap2);
 int num_set_pixels(int8_t pixel_byte);
 int floor_frac_pixel(long fractional_pixel_count);
@@ -44,7 +55,9 @@ int pitch_32bit(int width);
 void free_glyph_map(giko_glyph_map_t *map);
 void free_glyph_list(giko_glyph_t *list);
 void free_bitmap(giko_bitmap_t *bitmap);
+void free_unicode_str(giko_unicode_str_t *unicode_str);
 void print_bitmap(giko_bitmap_t *bitmap);
+void print_unicode_str(giko_unicode_str_t *unicode_str);
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
@@ -59,30 +72,18 @@ int main(int argc, char *argv[]) {
     FT_New_Face(library, argv[1], 0, &face);
     FT_Set_Pixel_Sizes(face, 16, 16);
 
-    giko_bitmap_t *chunk = crop_bitmap(reference, 0, 0, 16, 16);
     
     giko_glyph_map_t *glyph_map = new_glyph_map(face, argv[2]);
-    giko_score_t best = best_match(chunk, glyph_map->advances[16]);
-    printf("%lu, %lf\n", best.codepoint, best.similarity);
+    giko_unicode_str_t *aa = new_art_str(reference, glyph_map, 0.3);
+    print_unicode_str(aa);
 
     FT_Done_Face(face);
     FT_Done_FreeType(library);
 
-    free_bitmap(chunk);
     free_bitmap(reference);
     free_glyph_map(glyph_map);
+    free_unicode_str(aa);
     return 0;
-}
-
-void print_bitmap(giko_bitmap_t *bitmap) {
-    for (int y = 0; y < bitmap->rows; y++) {
-        for (int x = 0; x < bitmap->width; x++) {
-            int byte = bitmap->data[y * bitmap->pitch + (x / 8)];
-            char pixel = (byte & (1 << (7 - (x % 8)))) ? '#' : '.';
-            printf("%c", pixel);
-        }
-        printf("\n");
-    }
 }
 
 giko_bitmap_t *new_image_bitmap(char *filepath) {
@@ -96,14 +97,14 @@ giko_bitmap_t *new_image_bitmap(char *filepath) {
 
     fseek(file, 18, SEEK_SET);
     fread(&image->width, 4, 1, file);
-    fread(&image->rows, 4, 1, file);
+    fread(&image->height, 4, 1, file);
 
     uint32_t image_data_offset;
     fseek(file, 10, SEEK_SET);
     fread(&image_data_offset, 4, 1, file);
 
     int pitch = pitch_32bit(image->width);
-    int image_size = pitch * image->rows;
+    int image_size = pitch * image->height;
     image->pitch = pitch;
 
     uint8_t *pixel_array = malloc(image_size);
@@ -111,8 +112,8 @@ giko_bitmap_t *new_image_bitmap(char *filepath) {
     fread(pixel_array, 1, image_size, file);
 
     // Flip image vertically
-    for (int row = 0; row < image->rows / 2; row++) {
-        int mirror = image->rows - row - 1;
+    for (int row = 0; row < image->height / 2; row++) {
+        int mirror = image->height - row - 1;
         int row1 = row * pitch;
         int row2 = mirror * pitch;
         for (int byte = 0; byte < pitch; byte++) {
@@ -129,25 +130,73 @@ giko_bitmap_t *new_image_bitmap(char *filepath) {
     return image;
 }
 
+giko_unicode_str_t *new_art_str(giko_bitmap_t *reference, giko_glyph_map_t *map, double greed) {
+    long *codepoints = calloc(MAX_ART_STR_LEN, sizeof(long));
+    int height = reference->height;
+    int width = reference->width;
+    int em_height = map->em_height;
+    int rows = (height + (em_height - 1)) / em_height; // Ceiling function
+
+    int num_patches = 0;
+    for (int row = 0; row < rows; row++) {
+        int x = 0;
+        while (x < width) {
+            int y = row * em_height;
+            giko_match_t best_match = best_scanline_match(reference, map, x, y, greed);
+            codepoints[num_patches] = best_match.codepoint;
+            x += best_match.advance + 1;
+            num_patches++;
+        }
+        codepoints[num_patches] = LINE_FEED;
+        num_patches++;
+    }
+
+    giko_unicode_str_t *unicode_str = malloc(sizeof(giko_unicode_str_t));
+    unicode_str->size = num_patches;
+    unicode_str->codepoints = codepoints;
+    return unicode_str;
+}
+
+giko_match_t best_scanline_match(giko_bitmap_t *reference, giko_glyph_map_t *map, int x, int y, double greed) {
+    giko_match_t best_match = {0};
+    int advance = map->size;
+    while (advance > 0 && best_match.similarity < greed) {
+        giko_glyph_t *list = map->advances[advance];
+        if (!list) {
+            advance--;
+            continue;
+        }
+        giko_bitmap_t *patch = crop_bitmap(reference, x, y, advance, map->em_height);
+        giko_match_t match = patch_match(patch, list);
+        if (match.similarity >= best_match.similarity) {
+            best_match.advance = advance;
+            best_match.codepoint = match.codepoint;
+            best_match.similarity = match.similarity;
+        }
+        advance--;
+    }
+    return best_match;
+}
+
 giko_bitmap_t *crop_bitmap(giko_bitmap_t *bitmap, int x_offset, int y_offset, int width, int height) {
     assert(x_offset >= 0);
     assert(y_offset >= 0);
     assert(width >= 0);
     assert(height >= 0);
 
-    giko_bitmap_t *crop = malloc(sizeof(giko_bitmap_t));
+    giko_bitmap_t *patch = malloc(sizeof(giko_bitmap_t));
     int pitch = pitch_32bit(width);
-    crop->width = width;
-    crop->rows = height;
-    crop->pitch = pitch;
-    crop->data = calloc(pitch * height, sizeof(int8_t));
+    patch->width = width;
+    patch->height = height;
+    patch->pitch = pitch;
+    patch->data = calloc(pitch * height, sizeof(int8_t));
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             int src_x = x + x_offset;
             int src_y = y + y_offset;
             
-            if (src_x < bitmap->width && src_y < bitmap->rows) {
+            if (src_x < bitmap->width && src_y < bitmap->height) {
                 int src_byte_index = src_y * bitmap->pitch + (src_x / 8);
                 int src_bit_index = src_x % 8;
                 int dst_byte_index = y * pitch + (x / 8);
@@ -157,12 +206,12 @@ giko_bitmap_t *crop_bitmap(giko_bitmap_t *bitmap, int x_offset, int y_offset, in
                 int8_t dst_bit_mask = (1 << (7 - dst_bit_index));
 
                 if (bitmap->data[src_byte_index] & src_bit_mask) {
-                    crop->data[dst_byte_index] |= dst_bit_mask;
+                    patch->data[dst_byte_index] |= dst_bit_mask;
                 }
             }
         }
     }
-    return crop;
+    return patch;
 }
 
 giko_glyph_map_t *new_glyph_map(FT_Face face, char *filepath) {
@@ -175,6 +224,7 @@ giko_glyph_map_t *new_glyph_map(FT_Face face, char *filepath) {
     giko_glyph_map_t *map = malloc(sizeof(giko_glyph_map_t));
     int max_advance = floor_frac_pixel(face->size->metrics.max_advance);
     map->size = max_advance;
+    map->em_height = floor_frac_pixel(face->size->metrics.height);
     map->advances = calloc(max_advance + 1, sizeof(giko_glyph_t *));
 
     char codepoint_str[16];
@@ -210,14 +260,14 @@ giko_bitmap_t *new_glyph_bitmap(FT_Face face, long codepoint) {
     FT_Bitmap *src_bitmap = &face->glyph->bitmap;
 
     int width = floor_frac_pixel(face->glyph->metrics.horiAdvance);
-    int rows = face->size->metrics.y_ppem;
+    int height = face->size->metrics.y_ppem;
     int pitch = pitch_32bit(width);
 
     giko_bitmap_t *bitmap = malloc(sizeof(giko_bitmap_t));
     bitmap->width = width;
     bitmap->pitch = pitch;
-    bitmap->rows = rows;
-    bitmap->data = calloc(bitmap->rows * pitch, sizeof(uint8_t));
+    bitmap->height = height;
+    bitmap->data = calloc(bitmap->height * pitch, sizeof(uint8_t));
 
     int ascent = floor_frac_pixel(face->size->metrics.ascender);
     int x_offset = face->glyph->bitmap_left;
@@ -228,7 +278,7 @@ giko_bitmap_t *new_glyph_bitmap(FT_Face face, long codepoint) {
             int dst_x = x + x_offset;
             int dst_y = y + y_offset;
 
-            if (dst_x < width && dst_y < rows) {
+            if (dst_x < width && dst_y < height) {
                 int src_byte_index = y * src_bitmap->pitch + (x / 8);
                 int src_bit_index = x % 8;
                 int dst_byte_index = dst_y * pitch + (dst_x / 8);
@@ -247,30 +297,30 @@ giko_bitmap_t *new_glyph_bitmap(FT_Face face, long codepoint) {
     return bitmap;
 }
 
-giko_score_t best_match(giko_bitmap_t *reference, giko_glyph_t *head) {
-    giko_score_t best = {0};
-    best.advance = head->advance;
+giko_match_t patch_match(giko_bitmap_t *reference, giko_glyph_t *head) {
+    giko_match_t best_match = {0};
+    best_match.advance = head->advance;
     giko_glyph_t *curr;
     for (curr = head; curr != NULL; curr = curr->next) {
         double similarity = bitmap_similarity(reference, curr->bitmap);
         // PERFORMANCE: Add greed
-        if (similarity >= best.similarity) {
-            best.similarity = similarity;
-            best.codepoint = curr->codepoint;
+        if (similarity >= best_match.similarity) {
+            best_match.similarity = similarity;
+            best_match.codepoint = curr->codepoint;
         }
     }
 
-    return best;
+    return best_match;
 }
 
 double bitmap_similarity(giko_bitmap_t *bitmap1, giko_bitmap_t *bitmap2) {
-    assert(bitmap1->rows == bitmap2->rows);
+    assert(bitmap1->height == bitmap2->height);
     assert(bitmap1->pitch == bitmap2->pitch);
 
     double total_set_pixels = 0;
     double overlapping_set_pixels = 0;
 
-    for (int i = 0; i < bitmap1->pitch * bitmap1->rows; i++) {
+    for (int i = 0; i < bitmap1->pitch * bitmap1->height; i++) {
         int8_t byte_1 = bitmap1->data[i];
         int8_t byte_2 = bitmap2->data[i];
         overlapping_set_pixels += num_set_pixels(byte_1 & byte_2);
@@ -307,6 +357,11 @@ void free_bitmap(giko_bitmap_t *bitmap) {
     free(bitmap);
 }
 
+void free_unicode_str(giko_unicode_str_t *unicode_str) {
+    free(unicode_str->codepoints);
+    free(unicode_str);
+}
+
 int pitch_32bit(int width) {
     return ((width + 31) / 32) * 4;
 }
@@ -326,3 +381,59 @@ int num_set_pixels(int8_t pixel_byte) {
     return count;
 }
 
+// https://stackoverflow.com/questions/38491380/how-to-print-unicode-codepoints-as-characters-in-c
+unsigned convert_unicode_to_utf8(uint8_t *utf8, uint32_t codepoint) {
+    if (codepoint <= 0x7F) {
+        utf8[0] = codepoint;
+        return 1;
+    }
+    if (codepoint <= 0x7FF) {
+        utf8[0] = 0xC0 | (codepoint >> 6);
+        utf8[1] = 0x80 | (codepoint & 0x3F);
+        return 2;
+    }
+    if (codepoint <= 0xFFFF) {
+        // detect surrogates
+        if (codepoint >= 0xD800 && codepoint <= 0xDFFF) return 0;
+            utf8[0] = 0xE0 | (codepoint >> 12);
+            utf8[1] = 0x80 | ((codepoint >> 6) & 0x3F);
+            utf8[2] = 0x80 | (codepoint & 0x3F);
+        return 3;
+    }
+    if (codepoint <= 0x10FFFF) {
+        utf8[0] = 0xF0 | (codepoint >> 18);
+        utf8[1] = 0x80 | ((codepoint >> 12) & 0x3F);
+        utf8[2] = 0x80 | ((codepoint >> 6) & 0x3F);
+        utf8[3] = 0x80 | (codepoint & 0x3F);
+        return 4;
+    }
+    return 0;
+}
+
+void print_bitmap(giko_bitmap_t *bitmap) {
+    for (int y = 0; y < bitmap->height; y++) {
+        for (int x = 0; x < bitmap->width; x++) {
+            int byte = bitmap->data[y * bitmap->pitch + (x / 8)];
+            char pixel = (byte & (1 << (7 - (x % 8)))) ? '#' : '.';
+            printf("%c", pixel);
+        }
+        printf("\n");
+    }
+}
+
+void print_unicode_str(giko_unicode_str_t *unicode_str) {
+    uint8_t utf8[4];
+
+    for (int i = 0; i < unicode_str->size; i++) {
+        uint32_t codepoint = (uint32_t)unicode_str->codepoints[i];
+        unsigned length = convert_unicode_to_utf8(utf8, codepoint);
+
+        if (length > 0) {
+            fwrite(utf8, 1, length, stdout);
+        } else {
+            fprintf(stderr, "Invalid codepoint: U+%04X\n", codepoint);
+        }
+    }
+
+    printf("\n");
+}
